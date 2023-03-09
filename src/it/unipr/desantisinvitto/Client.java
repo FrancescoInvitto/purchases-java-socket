5b3a4fe2b06b9f8f1022f0ce4586a620566517f6
Fabrizio De Santis <desantisfabri98@gmail.com>
2023-03-09 15:50:22 +0100

    nuova versione

--- src/it/unipr/desantisinvitto/Client.java
+++ src/it/unipr/desantisinvitto/Client.java
@@ -1,85 +1,100 @@
 package it.unipr.desantisinvitto;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Random;
 
 public class Client {
+	
+	private static final int SPORT = 4444;
+	private static final String SHOST = "localhost";
 	private static final int MAX_PRICE = 200;
 	private static final int MIN_PRICE = 10;
+	private int COUNT = 3;
 	
-	private Socket socket;
-	private BufferedReader bufferedReader;
-	private BufferedWriter bufferedWriter;
-	
-	public Client(Socket socket) {
+	public void run() {
 		try {
-			this.socket = socket;
-			this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-			this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public void listenForMessage() {
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				// TODO Auto-generated method stub
-				String msgFromServer;
-				while(socket.isConnected()) {
-					try {
-						msgFromServer = bufferedReader.readLine();
-						System.out.println(msgFromServer);
-						sendOffer();
-						
+			Socket client = new Socket(SHOST, SPORT);
+			ObjectOutputStream os = new ObjectOutputStream(client.getOutputStream());
+			ObjectInputStream is = null;
+			
+			Random random = new Random();
+			int offer;
+			int currPrice;
+			
+			while(COUNT>0) {
+				
+				if(is==null) {
+					is = new ObjectInputStream(new BufferedInputStream(client.getInputStream()));
+				}
+				
+				Object o = is.readObject();
+				
+				if(o instanceof Price) {
+					Price ps = (Price) o;
+					currPrice = ps.getPrice();
+					System.out.format("Received  %s from Server%n", currPrice);
+					if(currPrice==0) {
+						break;
 					}
-					catch(Exception e) {
-						close(socket, bufferedWriter, bufferedReader);
+					
+					offer = random.nextInt(MAX_PRICE-MIN_PRICE) + MIN_PRICE;
+					
+					Offer oc;
+					
+					if(offer>=currPrice) {
+						oc = new Offer(offer);
+						System.out.format("Client sends %s to the Server%n", offer);
+					}
+					else {
+						System.out.println("Price too high");
+						oc = new Offer(0);
+					}
+					os.writeObject(oc);
+					os.flush();
+				}
+				
+								
+				o = is.readObject();
+				
+				if(o instanceof Integer) {
+					int result = ((Integer) o).intValue();
+					if(result==1) {
+						System.out.println("Offer accepted");
+						COUNT--;
+					}
+					else if(result==0) {
+						System.out.println("Offer declined");
 					}
 				}
+				
+				if(COUNT==0) {
+					os.writeObject(false);
+					os.flush();
+					break;
+				}
+				os.writeObject(true);
+				os.flush();
 			}
-		}).start();
-	}
-	
-	public void sendOffer() {
-		try {
-			bufferedWriter.write("prezzo ricevuto");
-			bufferedWriter.newLine();
-			bufferedWriter.flush();
+			
+			client.close();
 		}
-		catch (Exception e){
-			close(socket, bufferedWriter, bufferedReader);
-		}
-	}
-	
-	public void close(Socket socket, BufferedWriter bufferedWriter, BufferedReader bufferedReader) {
-		try {
-			if(bufferedReader!=null) {
-				bufferedReader.close();
-			}
-			if(bufferedWriter!=null) {
-				bufferedWriter.close();
-			}
-			if(socket!=null) {
-				socket.close();
-			}
-		}
-		catch(Exception e) {
+		catch(IOException | ClassNotFoundException e){
 			e.printStackTrace();
 		}
 	}
 	
-	public static void main(final String[] args) throws Exception {
-		Random random = new Random();
-		Socket socket = new Socket("localhost", 1000);
-		Client client = new Client(socket);
-		client.listenForMessage();
+	public static void main(final String[] args) {
+		new Client().run();
+		System.out.println("Connection closed");
 	}
 }
--- src/it/unipr/desantisinvitto/Server.java
+++ src/it/unipr/desantisinvitto/Server.java
@@ -2,6 +2,10 @@ package it.unipr.desantisinvitto;
 
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import java.net.Socket;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -9,36 +13,62 @@ import java.io.IOException;
 import java.net.ServerSocket;
 
 public class Server {
-	private static final int PORT = 1000;
-	private static final int MAX_PRICE = 200;
-	private static final int MIN_PRICE = 10;
-	private ServerSocket sSocket;
 	
-	public Server(ServerSocket sSocket) {
-		this.sSocket = sSocket;
+	private static final int COREPOOL = 3;
+	private static final int MAXPOOL = 3;
+	private static final int SPORT = 4444;
+	private static final long IDLETIME = 5000;
+	
+	private ServerSocket socket;
+	private ThreadPoolExecutor pool;
+	private ArrayList<ServerThread> serverThreads;
+	
+	public Server() throws IOException{
+		this.socket = new ServerSocket(SPORT);
+		this.serverThreads = new ArrayList<>();
 	}
 	
-	public void startServer() {
-		System.out.println("Server in ascolto...");
-		try {
-			while(!sSocket.isClosed()) {
-				Socket socket = sSocket.accept();
-				System.out.println("Nuovo client connesso: " + socket);
-				ClientHandler clientHandler = new ClientHandler(socket);
-				Thread thread = new Thread(clientHandler);
-				thread.start();
+	private void run() {
+		this.pool = new ThreadPoolExecutor(COREPOOL, MAXPOOL, IDLETIME, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>());
+		while(serverThreads.size()<3) {
+			System.out.println("Clients connected: " + serverThreads.size());
+			try {
+				Socket s = this.socket.accept();
+				serverThreads.add(new ServerThread(this, s));
+			}
+			catch(Exception e) {
+				break;
 			}
 		}
-		catch(Exception e) {
-			e.printStackTrace();
+		for(ServerThread serverThread : serverThreads) {
+			this.pool.execute(serverThread);
 		}
+		while(true) {
+			System.out.println("Clients connected: " + serverThreads.size());
+			try {
+				Socket s = this.socket.accept();
+				ServerThread serverThread = new ServerThread(this, s);
+				serverThreads.add(serverThread);
+				this.pool.execute(serverThread);
+			}
+			catch(Exception e) {
+				break;
+			}
+		}
+		this.pool.shutdown();
 	}
 	
-	public void closeServerSocket() {
+	public ThreadPoolExecutor getPool() {
+		return this.pool;
+	}
+	
+	public ArrayList<ServerThread> getList(){
+		return this.serverThreads;
+	}
+	
+	public void close() {
 		try {
-			if(sSocket!=null) {
-				sSocket.close();
-			}
+			this.socket.close();
 		}
 		catch(Exception e) {
 			e.printStackTrace();
@@ -46,8 +76,7 @@ public class Server {
 	}
 	
 	public static void main(String[] args) throws IOException{
-		ServerSocket serverSocket = new ServerSocket(PORT);
-		Server server = new Server(serverSocket);
-		server.startServer();
+		new Server().run();
+		System.out.println("Connection closed");
 	}
 }
